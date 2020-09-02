7b2bdd000599ce017770f324c2ac17f6098ebf8f
Cody Moore <cody@bloomcred.it>
2020-09-02 13:47:11 -0400

    fix: use forked version and update aws provider

--- main.tf
+++ main.tf
@@ -170,7 +170,7 @@ data "aws_iam_instance_profile" "default" {
 }
 
 module "autoscale_group" {
-  source = "git::https://github.com/cloudposse/terraform-aws-ec2-autoscale-group.git?ref=tags/0.7.1"
+  source = "git::github.com/dotCipher/terraform-aws-ec2-autoscale-group.git?ref=master"
 
   enabled    = var.enabled
   namespace  = var.namespace
